954d78b4b1fdcf957d0f6d8ee937f874855cdefa
Fahim Ali Zain <fahimalizain@gmail.com>
2022-02-17 21:13:33 +0530

    feat: Unit Sale Statuses

--- kallat_erpnext/kallat_erpnext/doctype/unit_sale/unit_sale.py
+++ kallat_erpnext/kallat_erpnext/doctype/unit_sale/unit_sale.py
@@ -1,8 +1,22 @@
 # Copyright (c) 2022, Fahim Ali Zain and contributors
 # For license information, please see license.txt
 
+from enum import Enum
+
 # import frappe
 from frappe.model.document import Document
 
+
+class UnitSaleStatuses(Enum):
+    BOOKED = "Booked"
+    AGREEMENT_SIGNED = "Agreement Signed"
+    EMPTY_PLOT = "Empty Plot"
+    FOUNDATION_COMPLETED = "Foundation Completed"
+    FIRST_FLOOR_SLAB_COMPLETED = "1st Floor Slab Completed"
+    STRUCTURE_COMPLETED = "Structure Completed"
+    TILING_COMPLETED = "Tiling Completed"
+    HAND_OVER_COMPLETED = "Hand Over Completed"
+
+
 class UnitSale(Document):
-	pass
+    pass
