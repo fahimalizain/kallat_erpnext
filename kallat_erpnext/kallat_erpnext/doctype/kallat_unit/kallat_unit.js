50e5aa68b0204d0314d7d4101904e6bd4973c112
Fahim Ali Zain <fahimalizain@gmail.com>
2022-02-17 21:09:19 +0530

    feat: Update Kallat Unit Statuses

--- kallat_erpnext/kallat_erpnext/doctype/kallat_unit/kallat_unit.js
+++ kallat_erpnext/kallat_erpnext/doctype/kallat_unit/kallat_unit.js
@@ -1,8 +1,60 @@
 // Copyright (c) 2022, Fahim Ali Zain and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Kallat Unit', {
-	// refresh: function(frm) {
+frappe.provide("kallat");
+kallat.unit_statuses = [
+  "Empty Plot",
+  "Foundation Completed",
+  "1st Floor Slab Completed",
+  "Structure Completed",
+  "Tiling Completed",
+  "Hand Over Completed",
+];
 
-	// }
+frappe.ui.form.on("Kallat Unit", {
+  refresh(frm) {
+    frm.events.add_update_status_button(frm);
+  },
+
+  add_update_status_button(frm) {
+    frm.add_custom_button("Update Status", () => {
+      const d = new frappe.ui.Dialog({
+        title: "Update Unit Status",
+        fields: [
+          {
+            label: "New Status",
+            fieldtype: "Select",
+            fieldname: "new_status",
+            options: kallat.unit_statuses.join("\n"),
+            default: frm.doc.status,
+          },
+          {
+            label: "Remarks",
+            fieldtype: "Small Text",
+            fieldname: "remarks",
+          },
+        ],
+        primary_action_label: "Update",
+        primary_action(values) {
+          frm.call({
+            method: "update_status",
+            doc: frm.doc,
+            args: values,
+            callback(r) {
+              if (r.exc) {
+                frappe.msgprint(
+                  "Something went wrong! Please try again or Contact Developer"
+                );
+              } else {
+                frappe.msgprint("Status Updated!");
+                d.hide();
+              }
+            },
+          });
+        },
+      });
+
+      d.show();
+    });
+  },
 });
--- kallat_erpnext/kallat_erpnext/doctype/kallat_unit/kallat_unit.py
+++ kallat_erpnext/kallat_erpnext/doctype/kallat_unit/kallat_unit.py
@@ -1,8 +1,44 @@
 # Copyright (c) 2022, Fahim Ali Zain and contributors
 # For license information, please see license.txt
 
-# import frappe
+from enum import Enum
+
+import frappe
 from frappe.model.document import Document
+from frappe.utils import cint, now_datetime
+
+
+class KallatUnitStatus(Enum):
+    EMPTY_PLOT = "Empty Plot"
+    FOUNDATION_COMPLETED = "Foundation Completed"
+    FIRST_FLOOR_SLAB_COMPLETED = "1st Floor Slab Completed"
+    STRUCTURE_COMPLETED = "Structure Completed"
+    TILING_COMPLETED = "Tiling Completed"
+    HAND_OVER_COMPLETED = "Hand Over Completed"
+
 
 class KallatUnit(Document):
-	pass
+    def validate(self):
+        self.validate_status_change()
+
+    def validate_status_change(self):
+        statuses = [e.value for e in KallatUnitStatus]
+        if self.status not in statuses:
+            raise Exception("Invalid Status")
+
+        if self.is_new():
+            return
+        if not cint(self.flags.allow_status_change):
+            raise Exception("Invalid Op")
+
+    @frappe.whitelist()
+    def update_status(self, new_status: str, remarks: str = None):
+        self.flags.allow_status_change = True
+        self.append("status_updates", dict(
+            date_time=now_datetime(),
+            remarks=remarks,
+            prev_status=self.status,
+            new_status=new_status
+        ))
+        self.status = new_status
+        self.save()
